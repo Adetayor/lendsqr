20c721e05f09a4012fc8d8ec2234c4c4e1f00bf0
Adetayor <adetayo456@gmail.com>
2024-12-04 12:52:20 +0100

    lendsqr login

---
+++ app/(auth)/(login)/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <main className='login_container'>
+      {/* logo */}
+      <h1>logo</h1>
+      {/* main section */}
+      <section>
+        <aside>
+          left
+        </aside>
+        <aside>
+          right
+        </aside>
+      </section>
+    
+    </main>
+  )
+}
+
+export default Login
