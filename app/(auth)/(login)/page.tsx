8fa28f9f2a38f2e354aef39a26a63ddc1eb1a48e
Adetayor <adetayo456@gmail.com>
2024-12-05 16:07:58 +0100

    lendsqr landing page

--- app/(auth)/(login)/page.tsx
+++ app/(auth)/(login)/page.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import styles from './login.module.scss'
 
 function Login() {
   return (
-    <main className='login_container'>
+    <main className={styles.login_container}>
       {/* logo */}
       <h1>logo</h1>
       {/* main section */}
       <section>
-        <aside>
-          left
+        <aside className={styles.left_container}>
         </aside>
-        <aside>
-          right
+        <aside className={styles.right_container}>
+          <div>
+            <h3>Welcome!</h3>
+            <span>Enter details to login.</span>
+            <div></div>
+          </div>
         </aside>
       </section>
     
